8470eaebf9d545decb962d7d80e10b0fe98bfc26
486868297 <58921962+486868297@users.noreply.github.com>
2020-09-29 09:08:57 +0800

    注册页面响应式布局

---
+++ Day16/js/components.js
@@ -0,0 +1,10 @@
+/*响应式导航条：点击汉堡包按钮时导航菜单从左侧进入 反之点击关闭按钮导航隐藏在左侧*/
+document.querySelector('[data-toggle="nav"]').onclick=function(){
+	document.querySelector('[data-target="nav"]').classList.add('active')
+}
+
+document.querySelector('[data-click="nav"]').onclick=function(){
+	document.querySelector('[data-target="nav"]').classList.remove('active')
+}
+
+
